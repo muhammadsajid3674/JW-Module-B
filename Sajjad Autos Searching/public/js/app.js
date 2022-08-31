64bd86ea6e9069bfd71fce0e11c7e38cb47f54e4
Sajid Shahid <sajiddark3674@gmail.com>
2022-08-31 19:16:19 +0500

    imgs added

--- Sajjad Autos Searching/public/js/app.js
+++ Sajjad Autos Searching/public/js/app.js
@@ -6,6 +6,7 @@ let car = {
             "Max Horsepower/RPM": "67 hp @ 6000 RPM",
             "Transmission": "Manual",
             "Fuel Capacity (Litres)": "35 L",
+            img: "Cultus.jpg",
         },
         "Wagon R AGS 2022": {
             "Engine Displacement": "998 cc",
@@ -13,6 +14,7 @@ let car = {
             "Max Horsepower/RPM": "67 hp @ 6200 RPM",
             "Transmission": "Manual",
             "Fuel Capacity (Litres)": "35 L",
+            img: "WagonR.jpg",
         },
         "Alto VXL 2022": {
             "Engine Displacement": "658 cc",
@@ -20,6 +22,7 @@ let car = {
             "Max Horsepower/RPM": "39 hp @ 6500 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "27 L",
+            img: "Alto.jpg",
         },
         "Swift DLX 1.3L 2022": {
             "Engine Displacement": "1328 cc",
@@ -27,6 +30,7 @@ let car = {
             "Max Horsepower/RPM": "90 hp @ 6000 RPM",
             "Transmission": "Manual",
             "Fuel Capacity (Litres)": "43 L",
+            img: "Swift.jpg",
         },
     },
     Honda: {
@@ -36,6 +40,7 @@ let car = {
             "Max Horsepower/RPM": "138 hp @ 6500 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "47 L",
+            img: "civic.png",
         },
         "City 1.5L ASPIRE CVT 2022": {
             "Engine Displacement": "1497 cc",
@@ -43,6 +48,7 @@ let car = {
             "Max Horsepower/RPM": "118 hp @ 6600 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "40 L",
+            img: "City.jpg",
         },
         "BR-V i-VTEC MT 2022": {
             "Engine Displacement": "1498 cc",
@@ -50,6 +56,7 @@ let car = {
             "Max Horsepower/RPM": "117 hp @ 6600 RPM",
             "Transmission": "Manual",
             "Fuel Capacity (Litres)": "42 L",
+            img: "BRV.jpg",
         },
     },
     Hyundai: {
@@ -59,6 +66,7 @@ let car = {
             "Max Horsepower/RPM": "154 hp @ 6200 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "50 L",
+            img: "Elantra.jpg",
         },
         "Tucson AWD A/T Ultimate 2022": {
             "Engine Displacement": "1999 cc",
@@ -66,6 +74,7 @@ let car = {
             "Max Horsepower/RPM": "155 hp @ 6200 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "62 L",
+            img: "Tucson.jpg",
         },
         "Sonata Fe GLS 2.4L 2022": {
             "Engine Displacement": "2400  cc",
@@ -73,6 +82,7 @@ let car = {
             "Max Horsepower/RPM": "170 hp @ 6000 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "71 L",
+            img: "Santa.jpg",
         },
     },
     Kia: {
@@ -82,6 +92,7 @@ let car = {
             "Max Horsepower/RPM": "154 hp @ 6200 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "62 L",
+            img: "sportage.jpg",
         },
         "Sorento 3.5 FWD 2022": {
             "Engine Displacement": "3342 cc",
@@ -89,6 +100,7 @@ let car = {
             "Max Horsepower/RPM": "276 hp @ 6000 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "58 L",
+            img: "Sorento.jpg",
         },
         "Grand Carnival LX 2022": {
             "Engine Displacement": "3342 cc",
@@ -96,6 +108,7 @@ let car = {
             "Max Horsepower/RPM": "270 hp @ 6400 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "80 L",
+            img: "Carnival.jpg",
         },
     },
     Toyota: {
@@ -105,6 +118,7 @@ let car = {
             "Max Horsepower/RPM": "106 hp @ 6000 RPM",
             "Transmission": "Manual",
             "Fuel Capacity (Litres)": "42 L",
+            img: "Yaris.jpg",
         },
         "Aqua L 1.5L 2022": {
             "Engine Displacement": "1496 cc",
@@ -112,6 +126,7 @@ let car = {
             "Max Horsepower/RPM": "72 hp @ 4800 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "36 L",
+            img: "Aqua.jpg",
         },
         "Corolla Altis CVT-i 1.8L 2022": {
             "Engine Displacement": "1598 cc",
@@ -119,6 +134,7 @@ let car = {
             "Max Horsepower/RPM": "138 hp @ 6400 RPM",
             "Transmission": "Automatic",
             "Fuel Capacity (Litres)": "55 L",
+            img: "Altis.jpg",
         },
     },
 }
@@ -153,13 +169,14 @@ function features() {
     carDetails.classList.add('container', 'border','shadow', 'mt-3', 'p-4', 'rounded')
     carDetails.style.backgroundColor = "#fff"
     carDetails.innerHTML = ""
-    carDetails.innerHTML = `<h1 class="mb-3">${carCompany.value} ${carModal.value} Features:</h1>`
+    carDetails.innerHTML = `<h1 class="mb-3">${carCompany.value} ${carModal.value} Features:</h1>
+    <img class="img-fluid" src="img/${modalFeaturesValues[5]}">`
     for (let i = 0; i < modalFeaturesKeys.length; i++) {
         carDetails.innerHTML += `
         <div class="features">
-            <ul>
-                <li><b>${modalFeaturesKeys[i]}</b> : ${modalFeaturesValues[i]}</li>
-            </ul>
+        <ul>
+        <li><b>${modalFeaturesKeys[i]}</b> : ${modalFeaturesValues[i]}</li>
+        </ul>
         </div>`
     }
 
