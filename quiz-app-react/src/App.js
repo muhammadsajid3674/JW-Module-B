b7f6ce0647333ffd8a398bfdde212d2bfdc3bf66
Sajid Shahid <sajiddark3674@gmail.com>
2022-10-06 22:12:11 +0500

    Quiz app done

---
+++ quiz-app-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizMain from './component/QuizMain/QuizMain';
+
+function App() {
+  return (
+    <>
+    <QuizMain/>
+    </>
+  );
+}
+
+export default App;
